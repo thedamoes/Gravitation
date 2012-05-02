522e30e876d7566a77c062ba4cdc972bd9a2fe66
cm226 <c.matear@gmail.com>
2012-05-02 13:55:46 +0100

    preliminery work on map editor

---
+++ MapEditor/MapEditor/EditorWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MapEditor
+{
+    /// <summary>
+    /// Interaction logic for EditorWindow.xaml
+    /// </summary>
+    public partial class EditorWindow : UserControl
+    {
+        public EditorWindow()
+        {
+            InitializeComponent();
+
+
+
+            // Create Image Element
+            Image myImage = new Image();
+            myImage.Width = 200;
+
+            // Create source
+            BitmapImage myBitmapImage = new BitmapImage();
+
+            // BitmapImage.UriSource must be in a BeginInit/EndInit block
+            myBitmapImage.BeginInit();
+            myBitmapImage.UriSource = new Uri(@"C:\Documents and Settings\All Users\Documents\My Pictures\Sample Pictures\Water Lilies.jpg");
+
+            // To save significant application memory, set the DecodePixelWidth or  
+            // DecodePixelHeight of the BitmapImage value of the image source to the desired 
+            // height or width of the rendered image. If you don't do this, the application will 
+            // cache the image as though it were rendered as its normal size rather then just 
+            // the size that is displayed.
+            // Note: In order to preserve aspect ratio, set DecodePixelWidth
+            // or DecodePixelHeight but not both.
+            myBitmapImage.DecodePixelWidth = 200;
+            myBitmapImage.EndInit();
+            //set image source
+            myImage.Source = myBitmapImage;
+
+            this.bla.Children.Add(myImage);
+        }
+    }
+}
---
+++ MapEditor/MapEditor/OptionsPannel.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MapEditor
+{
+    /// <summary>
+    /// Interaction logic for OptionsPannel.xaml
+    /// </summary>
+    public partial class OptionsPannel : UserControl
+    {
+
+        List<string> wallTypes = new List<string>();
+
+        
+
+
+        public OptionsPannel()
+        {
+            InitializeComponent();
+            initaliseGUI();
+        }
+
+        private void initaliseGUI()
+        {
+            wallTypes.Add("Left");
+            wallTypes.Add("Right");
+            wallTypes.Add("Bottom");
+            wallTypes.Add("Top");
+
+            this.Walls_type_select.ItemsSource = wallTypes;
+        }
+
+        private void wall_texture_bttn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                this.Walls_selected_texture.Text = filename;
+            }
+        }
+    }
+}
